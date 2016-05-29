26c0af810a120cf7ce546e9e0264cbfc6844d410
venusurampudi <svsvenu@gmail.com>
2016-05-28 21:37:52 -0500

    Added new file

--- src/main/java/com/venu/dockerjava.java
+++ src/main/java/com/venu/dockerjava.java
@@ -1,7 +1,22 @@
 package com.venu;
 
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.core.DockerClientBuilder;
+
 /**
  * Created by venusurampudi on 5/28/16.
  */
 public class dockerjava {
+
+    //nb1
+
+    public static void main(String args[]) {
+
+        DockerClient dockerClient = DockerClientBuilder.getInstance("http://localhost:2375").build();
+
+
+
+    }
+
+
 }
